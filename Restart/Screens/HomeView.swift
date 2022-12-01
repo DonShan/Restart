3b8ceae3d9016a4bdc79ade24a8be0bcebd5c879
DonShan <msdjshan47@gmail.com>
2022-12-01 18:01:54 +0530

    Updated header section

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -11,16 +11,29 @@ struct HomeView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isOnboardingViewActive = true
-            }) {
-                Text("restart")
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20){
+                Spacer()
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                         It's not how much we give but
+                         how much love we put into giving.
+                         """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }
             }
-        }//
+        }
     }
 }
 
