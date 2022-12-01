420dd94f176c3eebace43d3d60663440f1c50c8c
DonShan <msdjshan47@gmail.com>
2022-12-01 16:18:57 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Madushan Senavirathna on 2022-12-01.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
